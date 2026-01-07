2d53b46551a2d6c533b631455128109e55c6a214
André Pinheiro <drepimelo@hotmail.com>
2026-01-07 14:54:54 -0300

    a4

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    return {
+  const env = loadEnv(mode, '.', '');  
+  return {
       base: '/dalcontractorsinc/', // Substitua pelo nome real do seu repo no GitHub
       server: {
         port: 3000,
